19161495ad5db205e31a9de76a425b0d6c4e79f6
Lucas Avila <lucas.a.martins@ufv.br>
2018-05-02 17:14:51 -0300

    run prettier

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-declare module 'eh-dia-util-slim' {
-    export default function eh(d: Date | string): boolean;
+declare module "eh-dia-util-slim" {
+  export default function eh(d: Date | string): boolean;
 }
--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-'use strict';
+"use strict";
 
-var getBraziliansYearHolidays = require('./lib/brazilianHolidays');
-var parse = require('./lib/parse');
+var getBraziliansYearHolidays = require("./lib/brazilianHolidays");
+var parse = require("./lib/parse");
 
 var ehDiaUtil = function(date) {
-    var given = parse(date);
+  var given = parse(date);
 
-    var dayOfWeek = given.getDay();
-    var isSaturday = dayOfWeek === 6;
-    var isSunday = dayOfWeek === 0;
+  var dayOfWeek = given.getDay();
+  var isSaturday = dayOfWeek === 6;
+  var isSunday = dayOfWeek === 0;
 
-    if (isSaturday || isSunday) {
-        return false;
+  if (isSaturday || isSunday) {
+    return false;
+  }
+
+  var year = given.getFullYear();
+  var holidays = getBraziliansYearHolidays(year);
+  for (var i = 0; i < holidays.length; i++) {
+    var holiday = holidays[i];
+    var date = holiday.date.getDate();
+    var month = holiday.date.getMonth();
+
+    var givenDate = given.getDate();
+
+    var givenMonth = given.getMonth();
+
+    if (givenDate === date && givenMonth === month) {
+      return false;
     }
+  }
 
-    var year = given.getFullYear();
-    var holidays = getBraziliansYearHolidays(year);
-    for (var i = 0; i < holidays.length; i++) {
-        var holiday = holidays[i];
-        var date = holiday.date.getDate();
-        var month = holiday.date.getMonth();
-
-        var givenDate = given.getDate();
-
-        var givenMonth = given.getMonth();
-
-        if (givenDate === date && givenMonth === month) {
-            return false;
-        }
-    }
-
-    return true;
+  return true;
 };
 
 module.exports = ehDiaUtil;
